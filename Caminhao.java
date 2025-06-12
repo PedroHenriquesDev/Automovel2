d832150aa7544768e6d58833bddc51055cca24d9
PedroHenriquesDev <133235396+PedroHenriquesDev@users.noreply.github.com>
2025-06-12 16:39:29 -0300

    Update Caminhao.java

--- Caminhao.java
+++ Caminhao.java
@@ -1,14 +1,14 @@
 public class Caminhao extends Veiculo {
-    private double capacidadeCarga;
+private double capacidadeCarga;
 
-    public Caminhao(String placa, String modelo, int ano, double capacidadeCarga) {
-        super(placa, modelo, ano);
-        this.capacidadeCarga = capacidadeCarga;
-    }
+public Caminhao(String placa, String modelo, int ano, double capacidadeCarga) {
+    super(placa, modelo, ano);
+    this.capacidadeCarga = capacidadeCarga;
+}
 
     @Override
-    public void exibirDados() {
+        public void exibirDados() {
         System.out.println("Caminhão - Modelo: " + getModelo() + " | Placa: " + getPlaca() +
-                           " | Ano: " + getAno() + " | Capacidade de Carga: " + capacidadeCarga + " toneladas");
+        " | Ano: " + getAno() + " | Capacidade de Carga: " + capacidadeCarga + " toneladas");
     }
 }
